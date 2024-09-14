8a84ee3a93b435f8d2fa93e24206538648af519b
Ivan <108135420+IvanBanifatsyeu@users.noreply.github.com>
2024-09-14 23:22:25 +0300

    work with API, created service, pipe, work with @for and @if, interpolation, binding

---
+++ src/app/data/services/profile.service.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Profile } from '../interfaces/profile.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProfileService {
+hhtp: HttpClient = inject(HttpClient)
+
+baseApiUrl = 'https://icherniakov.ru/yt-course/'
+
+getTestAccounts(){
+ return this.hhtp.get<Profile[]>(`${this.baseApiUrl}account/test_accounts`)
+}
+}
