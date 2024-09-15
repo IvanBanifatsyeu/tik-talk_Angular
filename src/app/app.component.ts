898e392f1eb9115ad28efc4b93573e0681106189
Ivan <108135420+IvanBanifatsyeu@users.noreply.github.com>
2024-09-15 15:17:46 +0300

    routing layout login-page styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,24 +2,16 @@ import { Component, inject } from '@angular/core';
 
 import { RouterOutlet } from '@angular/router';
 import { ProfileCardComponent } from './common-ui/profile-card/profile-card.component';
-import { ProfileService } from './data/services/profile.service';
-import { Profile } from './data/interfaces/profile.interface';
+import { SearchPageComponent } from './pages/search-page/search-page.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ProfileCardComponent],
+  imports: [RouterOutlet, ProfileCardComponent,SearchPageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  profileService: ProfileService = inject(ProfileService);
-  profiles: Profile[] = [];
-
-  constructor() {
-    this.profileService.getTestAccounts().subscribe((val) => {
-      this.profiles = val;
-    });
-  }
+ 
 }
