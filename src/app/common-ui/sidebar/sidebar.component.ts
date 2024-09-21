2340d4e4b141d45ab0f91b39c76693caa5e3f646
Ivan <108135420+IvanBanifatsyeu@users.noreply.github.com>
2024-09-21 14:44:16 +0300

    svg like sprite, started coding sidebar

--- src/app/common-ui/sidebar/sidebar.component.ts
+++ src/app/common-ui/sidebar/sidebar.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SvgIconComponent } from "../svg-icon/svg-icon.component";
+import { SubscriberCardComponent } from "./subscriber-card/subscriber-card.component";
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [],
+  imports: [SvgIconComponent, CommonModule, SubscriberCardComponent],
   templateUrl: './sidebar.component.html',
-  styleUrl: './sidebar.component.scss'
+  styleUrl: './sidebar.component.scss',
 })
 export class SidebarComponent {
-
+  menuItems = [
+    {
+      label: 'My page',
+      icon: 'home',
+      link: '',
+    },
+    {
+      label: 'Chats',
+      icon: 'chats',
+      link: 'chats',
+    },
+    {
+      label: 'Search',
+      icon: 'search',
+      link: 'search',
+    },
+  ];
 }
